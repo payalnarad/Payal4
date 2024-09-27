42cd6e568a4e8719da17c6aca502448d6a414f6f
Samaksh <Samaksh@LAPTOP-UB65FEHH>
2024-09-27 11:54:32 +0530

    By Payal

---
+++ Jenkin_GitIntegration/src/Jenkin_github/Test.java
@@ -0,0 +1,28 @@
+package Jenkin_github;
+
+public class Test 
+{
+	public void m1() {
+		
+		System.out.println("This is m1 method");
+		
+	}
+
+	public void m2()
+	{
+		System.out.println("THis is Jenkin");
+		
+	}
+	
+	
+	public void m3()
+	{
+		System.out.println("THis is github");
+	}	
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+	
+}
